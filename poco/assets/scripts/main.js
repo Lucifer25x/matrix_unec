9b20974ff7287749641e7e10f7117a54157454a9
Lucifer25x <lucifer25x@protonmail.com>
2024-10-31 11:43:19 +0400

    Add sidebar

---
+++ poco/assets/scripts/main.js
@@ -0,0 +1,8 @@
+// Sidebar
+function openNav() {
+  document.getElementById("sidebar").classList.add("shown");
+}
+
+function closeNav() {
+  document.getElementById("sidebar").classList.remove("shown");
+}
