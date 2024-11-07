77a7e32e6e77d83e16ce9cc92bcab214d1029f74
Lucifer25x <lucifer25x@protonmail.com>
2024-11-07 22:45:02 +0400

    Update

---
+++ ecomall_part/assets/script/main.js
@@ -0,0 +1,26 @@
+// Dropdown elements
+const dropdownEl = document.getElementById("home-dropdown");
+document.getElementById("dropdown").addEventListener('mouseenter', () => {
+    dropdownEl.classList.add("shown");
+})
+document.getElementById("dropdown").addEventListener('mouseleave', () => {
+    dropdownEl.classList.remove("shown");
+})
+document.getElementById("dropdown2").addEventListener('mouseenter', () => {
+    dropdownEl.classList.add("shown");
+})
+document.getElementById("dropdown2").addEventListener('mouseleave', () => {
+    dropdownEl.classList.remove("shown");
+})
+document.getElementById("dropdown3").addEventListener('mouseenter', () => {
+    dropdownEl.classList.add("shown");
+})
+document.getElementById("dropdown3").addEventListener('mouseleave', () => {
+    dropdownEl.classList.remove("shown");
+})
+document.getElementById("dropdown4").addEventListener('mouseenter', () => {
+    dropdownEl.classList.add("shown");
+})
+document.getElementById("dropdown4").addEventListener('mouseleave', () => {
+    dropdownEl.classList.remove("shown");
+})
