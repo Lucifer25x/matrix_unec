3427718d3fcb1848c9339a384f9e99e809e9f947
Lucifer25x <lucifer25x@protonmail.com>
2025-01-03 14:44:56 +0400

    Update

--- homework/ovro_clone/src/App.jsx
+++ homework/ovro_clone/src/App.jsx
@@ -5,14 +5,28 @@ import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import FloatingSidebar from "./components/FloatingSidebar";
 import "./css/App.css";
+import sublogo from "./img/sublogo1.svg";
+import circle from "./img/circle.png";
+import reviews from "./img/reviews.png";
+import figma from "./img/figma.svg";
+import logo1 from "./img/logo1.png";
+import { RiArrowUpCircleFill } from "@remixicon/react";
 
 const App = () => {
   useEffect(() => {
     Aos.init();
   });
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
   return (
     <>
+      <div onClick={() => scrollToTop()} className="scroll_top"><RiArrowUpCircleFill size={50}/></div>
       <Navbar />
       <div className="container">
         <div className="left">
@@ -20,38 +34,132 @@ const App = () => {
           <Profile />
         </div>
         <div className="right">
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
-          <p  data-aos="fade-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, totam ab neque voluptatibus itaque nihil qui quaerat blanditiis sint aliquid temporibus nesciunt est doloremque non mollitia eligendi? Optio, doloribus perferendis!</p>
+          <p data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "1.5em", display: "flex", alignItems: "center", gap: "10px", fontWeight: "500"}}><img src={sublogo} width={20}/> Hi, I’m Alex, UI/UX Designer</p>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "4em", marginTop: "20px"}}>Designing Intuitive</h1>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{marginLeft: "40px", fontSize: "4em"}}>Experiences <span style={{fontFamily: "var(--font2)", fontStyle: "italic", color: "var(--primary)", textDecoration: "underline"}}>Inspire</span></h1>
+          <p data-aos-duration="1000" data-aos="fade-left" style={{width: "70%", float: "right", marginTop: "20px"}}>Your gateway to innovative design, seamless user experiences, and impactful solutions. Turning complex ideas into user-friendly designs.</p>
+          <div data-aos-duration="1000" data-aos="fade-left" style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "30px", float: "left"}}>
+            <img className="circle" src={circle} width={150}/>
+            <img src={reviews} width={150} />
+          </div>
+          <a data-aos-duration="1000" data-aos="fade-up" className="big_btn" href="#"><span>Lets Work Together</span></a>
+
+          <p data-aos-duration="1000" data-aos="fade-left" style={{marginTop: "50px", fontSize: "1.5em", display: "flex", alignItems: "center", gap: "10px", fontWeight: "500"}}><img src={sublogo} width={20}/> About Us</p>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "4em", marginTop: "20px"}}>Crafting Seamless</h1>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{marginLeft: "40px", fontSize: "4em"}}>User <span style={{fontFamily: "var(--font2)", fontStyle: "italic", color: "var(--primary)", textDecoration: "underline"}}>Experiences</span></h1>
+
+          <div className="cards">
+            <div data-aos-duration="1000" data-aos="fade-left" className="card">
+              <h1>Alex Carry</h1>
+              <p>Hi, I’m Alex Carry, a dedicated UI/UX Designer with 10 years of experience creating meaningful digital experiences. I specialize in crafting user-centered designs that enhance engagement and drive results. By combining research, creativity, and technical skills, I deliver solutions that not only look great but also function seamlessly. </p>
+            </div>
+            <div className="grid">
+              <div data-aos-duration="1000" data-aos="fade-up" className="card">
+                <h2>UI/UX Design</h2>
+                <p>Designing intuitive, user-focused interfaces that prioritize functionality & seamless navigation.</p>
+              </div>
+              <div data-aos-duration="1000" data-aos="fade-up" className="card">
+                <h2>User Research & Prototyping</h2>
+                <p>Designing intuitive, user-focused interfaces that prioritize functionality & seamless navigation.</p>
+              </div>
+              <div data-aos-duration="1000" data-aos="fade-up" className="card">
+                <h2>Responsive Web Design</h2>
+                <p>Designing intuitive, user-focused interfaces that prioritize functionality & seamless navigation.</p>
+              </div>
+              <div data-aos-duration="1000" data-aos="fade-up" className="card">
+                <h2>Wireframing & Visual Design</h2>
+                <p>Designing intuitive, user-focused interfaces that prioritize functionality & seamless navigation.</p>
+              </div>
+            </div>
+          </div>
+
+
+          <p data-aos-duration="1000" data-aos="fade-left" style={{marginTop: "50px", fontSize: "1.5em", display: "flex", alignItems: "center", gap: "10px", fontWeight: "500"}}><img src={sublogo} width={20}/> My Services</p>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "4em", marginTop: "20px"}}>Elevating Brands</h1>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{marginLeft: "40px", fontSize: "4em"}}>Intuitive <span style={{fontFamily: "var(--font2)", fontStyle: "italic", color: "var(--primary)", textDecoration: "underline"}}>Design</span></h1>
+          <div className="cards">
+            <div style={{marginTop: "20px"}} data-aos-duration="1000" data-aos="fade-up" className="card">
+              <h2>UI/UX Design</h2>
+              <p>Designing intuitive, user-focused interfaces that prioritize functionality and seamless navigation.</p>
+            </div>
+            <div style={{marginTop: "20px"}} data-aos-duration="1000" data-aos="fade-up" className="card">
+              <h2>User Research & Prototyping</h2>
+              <p>Conducting in-depth user research and creating interactive prototypes to validate ideas and refine designs.</p>
+            </div>
+            <div style={{marginTop: "20px"}} data-aos-duration="1000" data-aos="fade-up" className="card">
+              <h2>Responsive Web Design</h2>
+              <p>Crafting designs that adapt seamlessly across all devices, ensuring a consistent user experience.</p>
+            </div>
+            <div style={{marginTop: "20px"}} data-aos-duration="1000" data-aos="fade-up" className="card">
+              <h2>Wireframing & Visual Design</h2>
+              <p>Creating detailed wireframes and visually stunning designs to bring ideas into focus.</p>
+            </div>
+          </div>
+
+          <p data-aos-duration="1000" data-aos="fade-left" style={{marginTop: "50px", fontSize: "1.5em", display: "flex", alignItems: "center", gap: "10px", fontWeight: "500"}}><img src={sublogo} width={20}/> My Skill</p>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "4em", marginTop: "20px"}}>The Tools Behind</h1>
+          <h1 data-aos-duration="1000" data-aos="fade-left" style={{marginLeft: "40px", fontSize: "4em"}}>Exceptional <span style={{fontFamily: "var(--font2)", fontStyle: "italic", color: "var(--primary)", textDecoration: "underline"}}>Design</span></h1>
+
+          <div className="skills">
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+            <div data-aos-duration="1000" data-aos="fade-up" className="skill">
+              <img src={figma} width={80}/>
+              <h2>96%</h2>
+              <p>Figma</p>
+            </div>
+          </div>
+
+          {/* Add a few things if you have the time */}
+
+          <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+            <p data-aos-duration="1000" data-aos="fade-left" style={{marginTop: "50px", fontSize: "1.5em", display: "flex", alignItems: "center", gap: "10px", fontWeight: "500"}}><img src={sublogo} width={20}/> Contact Me</p>
+            <h1 data-aos-duration="1000" data-aos="fade-left" style={{fontSize: "4em", marginTop: "20px"}}>Have Any Project in</h1>
+            <h1 data-aos-duration="1000" data-aos="fade-left" style={{alignSelf: "flex-end", fontSize: "4em"}}>Mind, <span style={{fontFamily: "var(--font2)", fontStyle: "italic", color: "var(--primary)", textDecoration: "underline"}}>Lets Talk</span></h1>
+          </div>
+
+          <div className="contact">
+            <h1>Let's Work Together!</h1>
+            <div className="flex">
+              <input type="text" placeholder="Full Name"/>
+              <input type="text" placeholder="Email Adress"/>
+            </div>
+            <div className="flex">
+              <input type="text" placeholder="Phone Number"/>
+              <input type="text" placeholder="Subject"/>
+            </div>
+            <input type="text" placeholder="Your Budget (optional)"/>
+            <textarea placeholder="Message"></textarea>
+            <a href="#">SEND</a>
+          </div>
+
+          <div className="footer">
+            <img src={logo1} width={150}/>
+            <p>© 2024 OVRO. All Rights Reserved.</p>
+          </div>
         </div>
       </div>
     </>
--- homework/ovro_clone/src/components/Profile.jsx
+++ homework/ovro_clone/src/components/Profile.jsx
@@ -4,7 +4,7 @@ import profile_img from "../img/profile.png";
 
 const Profile = () => {
     return (
-        <div className="profile" data-aos="fade-right" data-aos-duration="1000">
+        <div className="profile" data-aos-once="true" data-aos="fade-right" data-aos-duration="1000">
             <div className="img">
                 <img src={profile_img}/>
             </div>
