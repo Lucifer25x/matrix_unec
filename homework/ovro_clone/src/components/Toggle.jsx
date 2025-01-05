f64cca0942e12cefc85107cab63a493fb1d37b41
Lucifer25x <lucifer25x@protonmail.com>
2025-01-05 14:10:46 +0400

    Save dark/light theme state

--- homework/ovro_clone/src/components/Toggle.jsx
+++ homework/ovro_clone/src/components/Toggle.jsx
@@ -2,7 +2,28 @@ import { useEffect, useState } from "react";
 import "../css/Toggle.css";
 
 const Toggle = () => {
-    const [isToggled, setIsToggled] = useState(false);
+    // const [isToggled, setIsToggled] = useState(false);
+
+    // useEffect(() => {
+    //     if (isToggled) {
+    //         document.body.classList.add("light");
+    //     } else {
+    //         document.body.classList.remove("light");
+    //     }
+    // }, [isToggled]);
+
+    const [isToggled, setIsToggled] = useState(() => {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme === "light";
+    })
+
+    const toggleTheme = () => {
+        setIsToggled(prevState => {
+            const newTheme = !prevState ? "light" : "dark";
+            localStorage.setItem("theme", newTheme);
+            return !prevState;
+        })
+    }
 
     useEffect(() => {
         if (isToggled) {
@@ -14,7 +35,8 @@ const Toggle = () => {
     
     return (
         <label className="toggler-wrapper style-1">
-            <input type="checkbox" onChange={() => setIsToggled(!isToggled)}/>
+            {/* <input type="checkbox" onChange={() => setIsToggled(!isToggled)}/> */}
+            <input type="checkbox" onChange={toggleTheme} checked={isToggled}/>
             <div className="toggler-slider">
                 <div className="toggler-knob"></div>
             </div>
