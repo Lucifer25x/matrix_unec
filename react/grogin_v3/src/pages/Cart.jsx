77ed9af631f004b7a65d2aa3b5ca7a282dc37617
Lucifer25x <lucifer25x@protonmail.com>
2025-01-19 11:56:56 +0400

    Small update

--- react/grogin_v3/src/pages/Cart.jsx
+++ react/grogin_v3/src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import { toast, Bounce, ToastContainer } from "react-toastify";
 import "../css/Cart.css";
 
 const Cart = () => {
-    const {isEmpty, items, emptyCart, removeItem, updateItemQuantity} = useCart();
+    const { isEmpty, items, emptyCart, removeItem, updateItemQuantity } = useCart();
 
     const handleRemoveItem = (id) => {
         removeItem(id);
@@ -55,19 +55,21 @@ const Cart = () => {
                         <tr key={index}>
                             <td>{index + 1}</td>
                             <td>{item.title}</td>
-                            <td><Price price={item.price}/>  </td>
+                            <td><Price price={item.price} />  </td>
                             <td>
-                                <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)}>-</button>
-                                {item.quantity}
-                                <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)} className="inc">+</button>
+                                <div className="btns">
+                                    <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)}>-</button>
+                                    {item.quantity}
+                                    <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)} className="inc">+</button>
+                                </div>
                             </td>
-                            <td><Price price={item.price * item.quantity}/></td>
+                            <td><Price price={item.price * item.quantity} /></td>
                             <td><button onClick={() => handleRemoveItem(item.id)}>Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-            <button className="clearAll" onClick={() => {handleClearCart()}} disabled={isEmpty}>Clear all the items</button>
+            <button className="clearAll" onClick={() => { handleClearCart() }} disabled={isEmpty}>Clear all the items</button>
 
             <ToastContainer />
         </div>
